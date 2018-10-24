e48539c4b72307ab9203031f1757b4bf384ea447
victoriavilasb <vick_vilasb@outlook.com>
2018-10-24 15:30:25 -0300

    Update 1168.rb
    Adicionando alguns comentários importantes

--- 1168.rb
+++ 1168.rb
@@ -4,6 +4,8 @@
     URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1168
 =end 
 number_times = gets
+
+#This hash represents the numbers of leds that you have to put in a number
 base_number_leds = {
     '1' => 2,
     '2' => 5,
@@ -23,10 +25,11 @@ while n<number_times.to_i
     total=0
     number_to_leds = gets.chomp
     number_to_leds.to_s
-
     number_to_leds_temporary_array = number_to_leds.split""
 
     i=0 
+    #We use the each position of the array to find the correct number of leds and we put this number in total to 
+    #sum the total of leds that we you need
     while i< (number_to_leds_temporary_array.length)
         number = number_to_leds_temporary_array[i]
         total=total+ base_number_leds[number]

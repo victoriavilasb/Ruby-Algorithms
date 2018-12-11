1ee6b1df9abf664f62bb2cd7a572e6e366520502
victoriavilasb <vick_vilasb@outlook.com>
2018-12-11 11:36:27 -0200

    Trocando while por for

--- 1168.rb
+++ 1168.rb
@@ -21,7 +21,7 @@ base_number_leds = {
 
 n=0
 number_times.to_i
-while n<number_times.to_i
+for num in (n,number_times.to_i)
     total=0
     number_to_leds = gets.chomp.to_s
     number_to_leds_temporary_array = number_to_leds.split""
@@ -29,15 +29,9 @@ while n<number_times.to_i
     i=0 
     #We use the each position of the array to find the correct number of leds and we put this number in total to 
     #sum the total of leds that we you need
-    while i< (number_to_leds_temporary_array.length)
-        number = number_to_leds_temporary_array[i]
+    for num in (i,number_to_leds_temporary_array.length)
+        number = number_to_leds_temporary_array[num]
         total+=base_number_leds[number]
-        i+=1
     end 
-
     puts total.to_s+' leds'
-
-    n+=1
 end 
-
-
